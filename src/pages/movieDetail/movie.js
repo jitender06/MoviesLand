714375e3fb4d6b44725f6aef1f98b5b74249babe
jitender <cjatinder39@gmail.com>
2023-06-13 04:06:05 -0700

    added babel

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -9,6 +9,7 @@ const Movie = () => {
   useEffect(() => {
     getData();
     window.scrollTo(0, 0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getData = () => {
