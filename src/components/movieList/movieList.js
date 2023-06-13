1d3549ec8ca576482af86f49f31c45cb0700547d
jitender <cjatinder39@gmail.com>
2023-06-13 03:58:39 -0700

    fixed error

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -1,41 +1,43 @@
-import React, {useEffect, useState} from "react"
-import "./movieList.css"
-import { useParams } from "react-router-dom"
-import Cards from "../card/card"
+import React, { useEffect, useState } from "react";
+import "./movieList.css";
+import { useParams } from "react-router-dom";
+import Cards from "../card/card";
 
 const MovieList = () => {
-    
-    const [movieList, setMovieList] = useState([])
-    const {type} = useParams()
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    useEffect(() => {
-        getData()
-    }, [])
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [type]);
 
-    useEffect(() => {
-        getData()
-    }, [type])
-
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=5ef178e79abecb1cd035030de4ea438c&language=en-US`)
-        .then(res => res.json())
-        .then(data => setMovieList(data.results))
-    }
-
-    return (
-        <div className="movie__list">
-            <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
-            <div className="list__cards">
-                {
-                    movieList.map(movie => (
-                        <div key={movie.id}>
-                          <Cards movie={movie}  />
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=5ef178e79abecb1cd035030de4ea438c&language=en-US`
     )
-}
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results));
+  };
 
-export default MovieList
+  return (
+    <div className="movie__list">
+      <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+      <div className="list__cards">
+        {movieList.map((movie) => (
+          <div key={movie.id}>
+            <Cards movie={movie} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
